60eb3571f76ace19cf8fb29e2066b4e2d1f73f10
isimrangupta <simrandgupta95@gmail.com>
2024-06-26 13:45:41 +0530

    Shoping-Cart added

---
+++ src/data.js
@@ -0,0 +1,33 @@
+let shopItemsData = [
+    {
+        id: 'dsjkfhdh',
+        name: "Black T-Shirt",
+        img: "images/Black T-Shirt.webp",
+        price: 60,
+        dce: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+
+    {
+        id: 'kdhhllsd',
+        name: "Casual T-Shirt",
+        img: "images/Casual Shirt.webp",
+        price: 100,
+        dce: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+
+    {
+        id: 'xckjdshkf',
+        name: "Formal T-Shirt",
+        img: "images/formal T-Shirt.webp",
+        price: 120,
+        dce: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+
+    {
+        id: 'fjhfdhk',
+        name: "White T-Shirt",
+        img: "images/img.webp",
+        price: 40,
+        dce: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+];
